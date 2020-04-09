724c6217327891b03206a5384999e82e9238520d
ptol <naughton.joshua@gmail.com>
2020-04-09 14:59:19 -0400

    Chapter 10 exercises

---
+++ 10_graphmod.mjs
@@ -0,0 +1,9 @@
+export function buildGraph(edges) {
+  let graph = Object.create(null);
+  function addEdge(from, to) { graph[from] == null ? graph[from] = [to] : graph[from].push(to); }
+  for (let [from, to] of edges.map(r => r.split("-"))) {
+    addEdge(from, to);
+    addEdge(to, from);
+  }
+  return graph;
+}
